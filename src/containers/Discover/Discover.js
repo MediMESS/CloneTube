983cb7295b532cc3a1d412b7ba78e59c2dc054a9
MediMESS <medimessarat@gmail.com>
2021-09-16 22:41:26 +0100

    Start working on Discover View

--- src/containers/Discover/Discover.js
+++ src/containers/Discover/Discover.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Tag from "../../components/UI/Tag/Tag";
 
-const Home = () => {
-  return <div>Discover</div>;
+const Discover = () => {
+  return (
+    <div>
+      <hr />
+      <h1>Tags</h1>
+      <hr />
+      <div>Discover</div>
+    </div>
+  );
 };
 
-export default Home;
+export default Discover;
